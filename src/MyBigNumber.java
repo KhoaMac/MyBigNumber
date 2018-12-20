6132229cb69c720732a4c44613dcc0a02136a91e
KhoaMac <leondk237@gmail.com>
2018-12-20 21:03:42 +0700

    Update - Fix coding convention and Upload unitest

--- src/MyBigNumber.java
+++ src/MyBigNumber.java
@@ -1,26 +1,33 @@
-import java.util.Scanner;
-import java.util.Stack;
 /**
  *
  * @author leond
  */
+
 /**
  * Sum two string class.
  */
 public class MyBigNumber {
+    
     private RememberStep rememberStep;
     
-    public MyBigNumber() {
-    }
+    public MyBigNumber() {}
     
     public MyBigNumber(final RememberStep rememberStep) {
         this.rememberStep = rememberStep;
     }
-
+    
+    /**
+      *sum function.
+      */
+    
     public String sumNumber(final String str1, final String str2) {
 
-        if (!str1.matches("^[0-9]+$") || !str2.matches("^[0-9]+$")) { // Check input: Number or String
-            return "Please input numeric string or positive number";
+        if (str1.contains("-")) {
+            throw new NumberFormatException("So thu nhat phai la so nguyen duong");
+        }
+
+        if (str2.contains("-")) {
+            throw new NumberFormatException("So thu nhi phai la so nguyen duong");
         }
 
         String finalResult = ""; // String contain the final result 
@@ -29,16 +36,36 @@ public class MyBigNumber {
         int getSum = 0; // variable get The Sum of two numbers
         int getUnit = 0;// variable get The number per unit
         int remember = 0; // variable remember The number when sumed greater than 9
+        int i = 0;
 
         int stringContain1 = 0; // variable get the number in String 1
         int stringContain2 = 0; // variable get the number in String 2
 
         int lengthStr1 = str1.length(); // variable contain the length of String 1
         int lengthStr2 = str2.length(); // variable contain the length of String 2
+        
+        char checkS1;
+        char checkS2;                          
 
-        int lengthContain = lengthStr1 < lengthStr2 ? lengthStr2 : lengthStr1; // variable contain the longest string of String 1 and String 2
+        int lengthContain = lengthStr1 < lengthStr2 ? lengthStr2 : lengthStr1; 
 
-        for (int i = 0; i < lengthContain; i++) {
+        for (i = 0; i < lengthContain; i++) {
+            
+            checkS1 = i < lengthStr1 ? str1.charAt(i) : '0';
+            checkS2 = i < lengthStr2 ? str2.charAt(i) : '0';
+
+            if (!(checkS1 >= '0' && checkS1 <= '9')) {
+                throw new NumberFormatException("Vi tri thu "
+                        + (str1.indexOf(checkS1) + 1)
+                        + " cua so thu 1 khong phai la so");
+            }
+
+            if (!(checkS2 >= '0' && checkS2 <= '9')) {
+                throw new NumberFormatException("Vi tri thu "
+                        + (str2.indexOf(checkS1) + 1)
+                        + " cua so thu 2 khong phai la so");
+            }
+            
             stringContain1 = i < lengthStr1 ? (str1.charAt(lengthStr1 - i - 1) - '0') : 0;
             stringContain2 = i < lengthStr2 ? (str2.charAt(lengthStr2 - i - 1) - '0') : 0;
 
@@ -51,7 +78,7 @@ public class MyBigNumber {
                         + " cong " + stringContain2
                         + " bang " + getSum + ""
                         + " viet " + getUnit
-                        + " nho " + getSum/10 + "\n";
+                        + " nho " + getSum / 10 + "\n";
             } else {
                 stepMsg += "\n" + "Buoc " + (i + 1) + ": "
                         + "lay " + stringContain1
@@ -64,11 +91,22 @@ public class MyBigNumber {
 
             remember = getSum / 10;
             finalResult = getUnit + finalResult;
-            if (remember > 0) {
-                lengthContain = lengthContain + 1;
-            } 
+            
         }
+        
+        if (remember > 0) {
+            finalResult = remember + finalResult;
+            stepMsg += "\n" + "Buoc " + (i + 1) + ": "
+                        + "lay " + 0
+                        + " cong " + 0
+                        + " nho " + 1
+                        + " bang " + 1
+                        + " viet " + 1
+                        + "\n";
+        } 
+        
         rememberStep.sendMessage(stepMsg);
+        
         return finalResult;
     }
 }
--- src/MyBigNumberMain.java
+++ src/MyBigNumberMain.java
@@ -1,10 +1,20 @@
+import java.util.Scanner;
+
 public class MyBigNumberMain implements RememberStep{
 
     public static void main(String[] args) {
 
         MyBigNumberMain main = new MyBigNumberMain();
         MyBigNumber s = new MyBigNumber(main);
-        String result = s.sumNumber(args[1], args[2]);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Nhap so thu nhat: ");
+        String num1 = sc.nextLine();
+        System.out.print("Nhap so thu hai: ");
+        String num2= sc.nextLine();
+
+        String result = s.sumNumber(num1, num2);
+        System.out.print("sum" + " " + num1 + " " + num2 + "\n");
         System.out.print("Ket Qua Cuoi Cung: " + result);
     }
 
